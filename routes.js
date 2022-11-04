cc7b08bf94f97efd638662e29308e3f0371e4119
Suraiya Hasan <83490460+Suraiya-Hasan@users.noreply.github.com>
2022-11-04 19:25:58 +0600

    passportjs authentication lab 05

--- routes.js
+++ routes.js
@@ -2,13 +2,36 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("./controllers/homeController");
 const bookController = require("./controllers/bookController");
+const connectEnsureLogin = require('connect-ensure-login');
+const passport = require('passport');
+
+
+
+
 
 router.get("/", homeController.getHome);
-router.get("/book-list", bookController.getBookList);
-router.get("/books", bookController.getBook);
-router.post("/books", bookController.postBook);
-router.post("/delete", bookController.deleteBook);
-router.get("/book-list/:id/edit", bookController.editBook);
-router.post("/book-list/:id", bookController.updateBook);
+router.get('/login', homeController.getLogin);
+router.get('/logout', homeController.logOut);
+router.get("/book-list", connectEnsureLogin.ensureLoggedIn(), bookController.getBookList);
+router.get("/books", connectEnsureLogin.ensureLoggedIn(), bookController.getBook);
+router.get('/dashboard', connectEnsureLogin.ensureLoggedIn(), homeController.getDashboard);
+router.post("/books", connectEnsureLogin.ensureLoggedIn(), bookController.postBook);
+router.get("/register", homeController.getRegister);
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login',
+    successRedirect: '/dashboard',
+  }), homeController.postLogin);
+
+// router.post(
+//   '/register',
+//   passport.authenticate('local', {
+//     failureRedirect: '/register',
+//     successRedirect: '/dashboard',
+//   }), homeController.postRegister);
+
+router.post(
+  '/register', homeController.postRegister);
 
 module.exports = router;
